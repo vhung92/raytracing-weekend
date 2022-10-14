08b6b2c5418c58306bcbef23003237ab94bd6bfb
Victor Hung <victor.hung@es-vhung.mbp>
2022-10-14 23:12:22 +0200

    rust fmt

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
-use std::sync::Arc;
+use crate::hitable::Hitable;
 use rand::random;
-use crate::hitable::{Hitable};
-use crate::vec3::Vec3;
-use crate::ray::Ray;
+use std::sync::Arc;
+use std::time::Instant;
 
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 
-mod vec3;
-mod ray;
-mod hitable;
-mod sphere;
-mod hitable_list;
 mod camera;
+mod hitable;
+mod hitable_list;
 mod materials;
+mod ray;
+mod sphere;
 mod utils;
+mod vec3;
 
 fn main() {
+    let startTime = Instant::now();
     env_logger::init();
     let nx = 800;
     let ny = 400;
@@ -23,19 +24,37 @@ fn main() {
     // Create a new ImgBuf with width: imgx and height: imgy
     let mut imgbuf = image::ImageBuffer::new(nx, ny);
 
-    let mut list: Vec<Box<dyn Hitable>> = Vec::new();
-    list.push(Box::new(sphere::new(vec3::new(0.0,0.0,-1.0), 0.5, Arc::new(materials::new_lambertian(vec3::new(0.8, 0.3, 0.3))))));
-    list.push(Box::new(sphere::new(vec3::new(0.0,-100.5,-1.0), 100.0, Arc::new(materials::new_lambertian(vec3::new(0.8, 0.8, 0.0))))));
-    list.push(Box::new(sphere::new(vec3::new(1.0,0.0,-1.0), 0.5, Arc::new(materials::new_metal(vec3::new(0.8, 0.6, 0.2))))));
-    list.push(Box::new(sphere::new(vec3::new(-1.0,0.0,-1.0), 0.5, Arc::new(materials::new_metal(vec3::new(0.8, 0.8, 0.8))))));
+    let list: Vec<Box<dyn Hitable>> = vec![
+        Box::new(sphere::new(
+            vec3::new(0.0, 0.0, -1.0),
+            0.5,
+            Arc::new(materials::new_lambertian(vec3::new(0.8, 0.3, 0.3))),
+        )),
+        Box::new(sphere::new(
+            vec3::new(0.0, -100.5, -1.0),
+            100.0,
+            Arc::new(materials::new_lambertian(vec3::new(0.8, 0.8, 0.0))),
+        )),
+        Box::new(sphere::new(
+            vec3::new(1.0, 0.0, -1.0),
+            0.5,
+            Arc::new(materials::new_metal(vec3::new(0.8, 0.6, 0.2))),
+        )),
+        Box::new(sphere::new(
+            vec3::new(-1.0, 0.0, -1.0),
+            0.5,
+            Arc::new(materials::new_metal(vec3::new(0.8, 0.8, 0.8))),
+        )),
+    ];
 
     let world: Box<dyn Hitable> = Box::new(hitable_list::new(list));
     let cam = camera::new();
+    info!("Generating image...");
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
         let mut col = vec3::new(0.0, 0.0, 0.0);
         for _ in 0..ns {
             let u = (x as f64 + random::<f64>()) / nx as f64;
-            let v = ((ny-y) as f64 + random::<f64>())  / ny as f64;
+            let v = ((ny - y) as f64 + random::<f64>()) / ny as f64;
             let ray = cam.get_ray(u, v);
             // let p = ray.point_at_parameter(2.0);
             col += utils::color(ray, &world, 0);
@@ -47,6 +66,9 @@ fn main() {
         *pixel = image::Rgb([ir, ig, ib]);
         debug!("x: {}, y: {}, color: {}", x, y, col);
     }
-
+    info!(
+        "Done generating image in {} seconds",
+        startTime.elapsed().as_secs()
+    );
     imgbuf.save("blah.png").unwrap();
 }
